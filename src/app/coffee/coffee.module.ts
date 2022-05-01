9354111229d2efdaa727859003b107d5c4697fec
csolanor22 <c.solanor@uniandes.edu.co>
2022-05-01 10:54:01 -0500

    Add basic files for class, service and component

---
+++ src/app/coffee/coffee.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ListCoffeeComponent } from './list-coffee/list-coffee.component';
+
+
+
+@NgModule({
+  declarations: [
+    ListCoffeeComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class CoffeeModule { }
